b7df391a68de036fb5bf1398901e52ce0f9575ee
EC2 Default User <ec2-user@ip-172-31-70-86.ec2.internal>
2021-11-24 09:43:48 +0000

    Add レビュー機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_004735) do
+ActiveRecord::Schema.define(version: 2021_11_24_083656) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_004735) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "rate"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -40,9 +41,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_004735) do
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
