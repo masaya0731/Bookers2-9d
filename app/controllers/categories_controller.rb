727da26ebe2491b89ef1605d8a1b94bd157941cb
EC2 Default User <ec2-user@ip-172-31-70-86.ec2.internal>
2021-12-05 13:30:55 +0000

    不要なディレクトリ削除

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,10 @@
 class CategoriesController < ApplicationController
   def index
-
     if params[:search]
       @books = Book.where("category LIKE ? ",'%' + params[:search] + '%')
     else
       @books = Book.where(category: :search)
     end
-
     @user = current_user
     @book = Book.new
   end
